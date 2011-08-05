7c94fb7d281415f927aec979d9bdc96fdf6c2a35
Thibaut Cuvelier <tcuvelier@redaction-developpez.com>
2011-08-05 22:03:57 +0200

    More pythonic

--- pyWeb2FOAF/Vocabularies/RDFTriple.py
+++ pyWeb2FOAF/Vocabularies/RDFTriple.py
@@ -1,27 +1,29 @@
-class RDFTriple: 	
-	_subject
-	_predicate
-	_object
-	
-	def __init__(self, subject, predicate, object):
+class RDFTriple: 		
+	def __init__(self, subject, predicate, obj):
 		self._subject   = subject
 		self._predicate = predicate
-		self._object    = object
-		
-	def setSubject(self, subject):
-		self._subject = subject
-		
-	def getSubject(self):
+		self._obj       = obj
+	
+	@property 
+	def subject(self):
 		return self._subject
-		
-	def setPredicate(self, predicate):
-		self._predicate = predicate
-		
-	def getPredicate(self):
+
+	@subject.setter
+	def subject(self, subject):
+                self._subject = subject
+
+	@property
+	def predicate(self):
 		return self._predicate
-		
-	def setObject(self, object):
-		self._object = object
-		
-	def getObject(self):
-		return self._object
+
+	@predicate.setter	
+	def predicate(self, predicate):
+		self._predicate = predicate
+
+	@property
+	def obj(self):
+		return self._obj
+
+	@obj.setter
+	def obj(self, obj):
+		self._obj = obj
